f2a476d98f1dec69771be34da80a3622ae657c13
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-31 12:08:43 -0400

    add bundles page

---
+++ public/app.js
@@ -0,0 +1,22 @@
+function getFruit() {
+  fetch('/fruit', {
+    headers: {
+      'x-requesting': 'fruit',
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error ${response.status}`)
+      }
+      return response.json()
+    })
+    .then((data) => {
+      document.getElementById('fruit').innerText = data.fruit
+    })
+    .catch((err) => {
+      console.error('error fetching fruit')
+      console.error(err)
+      document.getElementById('fruit').innerText = err.message
+    })
+}
+document.getElementById('load-fruit').addEventListener('click', getFruit)
